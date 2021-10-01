397c612a1e9bd950e1759903e2d2510aa48fe015
Dmytro Martus <funbrainfun@gmail.com>
2021-10-01 17:18:54 +0300

    local storage usage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -19,6 +28,19 @@ function App() {
       showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map((item) => {
+          console.log(item);
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
     } else {
       //  show alert
       showAlert(true, 'success', 'item added to the list');
@@ -47,6 +69,9 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
